8382b08f6447737cc5a41832e52a4decfac89571
limengjian <limengjian@51yund.com>
2021-11-11 20:41:43 +0800

    9.SecureJSON

--- 9.SecureJSON/main.go
+++ 9.SecureJSON/main.go
@@ -1,3 +1,24 @@
 package main
 
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
+func SecureJson(c *gin.Context) {
+	names := []string{"aab", "bba", "aabb"}
+
+	// 将输出: while(1);["aab", "bba", "aabb"]
+	c.SecureJSON(http.StatusOK, names)
+}
+
+func main() {
+	r := gin.Default()
+
+	// 也可以使用自己的SecureJSON前缀
+	// r.SecureJsonPrefix(")]}', \n")
+
+	r.GET("/securejson", SecureJson)
+
+	r.Run(":8080")
+}
