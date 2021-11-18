c1d33e5e78710efafc66a09a1c5ba8a18037f6e1
limengjian <limengjian@51yund.com>
2021-11-18 14:22:16 +0800

    记录日志文件

---
+++ 21.WriteLog/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+	// 禁用控制台颜色，将日志写入文件时不需要控制台颜色
+	gin.DisableConsoleColor()
+
+	// 记录到文件。
+	f, _ := os.Create("gin.log")
+	//gin.DefaultWriter = io.MultiWriter(f)
+
+	// 如果需要同时将日志写入文件和控制台，使用以下代码
+	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
+
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.String(http.StatusOK, "pong")
+	})
+
+	r.Run(":8080")
+}
