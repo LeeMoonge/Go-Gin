e4b2e84af0a5d34789b6801073f263624cf26750
limengjian <limengjian@51yund.com>
2021-11-19 17:19:45 +0800

    自定义中间件

--- 33.CostomMiddleware/main.go
+++ 33.CostomMiddleware/main.go
@@ -1,1 +1,42 @@
-package _3_CostomMiddleware
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+	"time"
+)
+
+func Logger() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		t := time.Now()
+
+		// 设置example变量
+		c.Set("example", "12345")
+
+		// 请求前
+		c.Next()
+
+		// 请求后
+		latency := time.Since(t)
+		log.Print(latency)
+
+		// 获取发送的status
+		status := c.Writer.Status()
+		log.Println(status)
+	}
+}
+
+func main()  {
+	r := gin.Default()
+	r.Use(Logger())
+
+	r.GET("/test", func(c *gin.Context) {
+		example := c.MustGet("example").(string)
+
+		// 打印："12345"
+		log.Println(example)
+	})
+
+	r.Run()
+
+}
