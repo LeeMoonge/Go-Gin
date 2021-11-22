bfe6f7f3f058ff60b2e5ce3d78816d0d604e7902
limengjian <limengjian@51yund.com>
2021-11-22 11:11:59 +0800

    设置和获取Cookie

--- 35.SetOrGetCookie/main.go
+++ 35.SetOrGetCookie/main.go
@@ -1,1 +1,21 @@
-package _5_SetOrGetCookie
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/cookie", func(c *gin.Context) {
+		cookie, err := c.Cookie("gin_cookie")
+
+		if err != nil {
+			cookie = "NotSet"
+			c.SetCookie("gin_cookie", "test", 3600, "/", "localhost", false, true)
+		}
+		fmt.Println("Cookie value:", cookie)
+	})
+	r.Run()
+}
